1c152214798c85fb4ba66600d5f1f7ae84368899
Saurabh sharma <srbsrm2@gmail.com>
2024-01-15 01:06:22 +0530

    Tetris

---
+++ lib/pixel.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Pixel extends StatelessWidget {
+  final color;
+
+  const Pixel({super.key, required this.color,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(4)),
+      margin: const EdgeInsets.all(1),
+    );
+  }
+}
